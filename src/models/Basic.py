30770abfd7830be5e655786b26ed4ce4ccf80df6
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2020-03-16 17:06:49 +0100

    Dont use nn.LogSoftmax

--- src/models/Basic.py
+++ src/models/Basic.py
@@ -6,13 +6,12 @@ class Basic(nn.Module):
     super(Basic, self).__init__()
     self.c1 = nn.Conv1d(1, 128, 32)
     self.c2 = nn.Conv1d(128, n_classes, 64)
-    self.log_softmax = nn.LogSoftmax(dim = 1)
 
   def forward(self, X):
     a = self.c1(X)
     b = F.relu(a)
     c = self.c2(b)
-    return self.log_softmax(c, dim=1)
+    return F.log_softmax(c, dim=1)
 
   def forward_shape(self, lengths):
     return lengths - 32 - 64 + 1 + 1
